7f01c44cf51f881ccb846f972b9a2278c61e67e1
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-03-16 10:50:10 +0800

    bug changed from obvious bug to wrong PORT

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.listen(PORT, () => {
-  this_causes_error
+app.listen(PORT + 1, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
